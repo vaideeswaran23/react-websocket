70fd1ac0ca5620a2eda91afcfe66bc42f445d311
Vaideeswaran C <vaideeswaran@agentz.ai>
2020-11-28 12:52:30 +0530

    Changed socket host

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
                 <div className="align-center">
                     {this.displayMessages()}
                 </div>
-                <SockJsClient url='http://localhost:8080/websocket-chat/'
+                <SockJsClient url='https://crimsonchat.herokuapp.com/websocket-chat/'
                               topics={['/topic/user']}
                               onConnect={() => {
                               }}
