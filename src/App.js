24a45995a4dfaef9606700e3d4976ae5cc47b572
Vaidee <vaideeswaran@agentz.ai>
2020-12-05 10:34:25 +0530

    Added video support - part 2

--- src/App.js
+++ src/App.js
@@ -7,35 +7,39 @@ import './css/MessageStyle.css';
 import NameComponent from "./components/NameComponent";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Icon from '@material-ui/core/Icon';
+import Peer from "simple-peer";
+import VideocamIcon from '@material-ui/icons/Videocam';
+// const configuration = {
+//     iceServers: [
+//       {
+//         urls: [
+//           'stun:stun1.l.google.com:19302',
+//           'stun:stun2.l.google.com:19302',
+//         ],
+//       },
+//     ],
+//     iceCandidatePoolSize: 10,
+//   };
 
-const configuration = {
-    iceServers: [
-      {
-        urls: [
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-        ],
-      },
-    ],
-    iceCandidatePoolSize: 10,
-  };
-
-let peerConnection = null;
-
-let remoteStream = null;
+let callerPeer;
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.videoRef = React.createRef();
+        this.userVideoRef = React.createRef();
         this.remoteVideoRef = React.createRef();
         this.state = {
             messages: [],
             typedMessage: "",
             name: "",
-            stream: null,
-            isCaller: null
+            isVideoEnabled: false,
+            isCaller: false,
+            isCallReceived: false,
+            receiverName: "",
+            receivedSignal: "",
+            isCallAnswered: false,
+            joined: false
         }
     }
 
@@ -50,13 +54,6 @@ class App extends Component {
         }
     }
 
-    componentDidUpdate() {
-        console.log("component update is called", remoteStream);
-        if(remoteStream != null) {
-            this.remoteVideoRef.current.srcObject = remoteStream
-        }
-    }
-
     sendMessage = () => {
         this.clientRef.sendMessage('/app/user-all', JSON.stringify({
             name: this.state.name,
@@ -65,19 +62,13 @@ class App extends Component {
         this.setState({typedMessage: ''})
     };
 
-    sendOffer = (offer) => {
-        this.clientRef.sendMessage('/app/video-all', JSON.stringify({
-            sdp: offer.sdp,
-            type: offer.type
-        }));
-    }
 
     displayMessages = () => {
         return (
             <div>
-                {this.state.messages.map(msg => {
+                {this.state.messages.map((msg, index) => {
                     return (
-                        <div>
+                        <div key={index}>
                             {this.state.name === msg.name ?
                                 <div>
                                     <p className="title1">{msg.name} : </p><br/>
@@ -94,101 +85,113 @@ class App extends Component {
         );
     };
 
-    handleOnClick = async event => {
-        event.preventDefault();
-        const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-        this.videoRef.current.srcObject = stream;
-        this.setState({stream})
+    //Video call part
+    getUserMedia = () => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then( stream => {
+            this.setState({ isVideoEnabled: true });
+            this.setState({ stream })
+            if(this.userVideoRef.current) {
+                this.userVideoRef.current.srcObject = stream;
+            }
+        })
     }
 
-    handleCall = async event => {
-        event.preventDefault();
-        const {stream} = this.state;
-        this.setState({isCaller: true})
-        
-        peerConnection = new RTCPeerConnection(configuration);
-        
-        peerConnection.addStream(stream);
-
-        this.registerPeerConnectionListeners();
-        
-        const offer = await peerConnection.createOffer();
-        
-        await peerConnection.setLocalDescription(offer);
-        console.log("created offer", offer);
-        this.sendOffer(offer);
+    startCall = () => {
+        const { stream, name } = this.state;
+        if(!stream) {
+            alert("Enable Video to start call");
+            return;
+        }
+        this.setState({ isCaller: true })
+        const peer = new Peer({
+            initiator: true,
+            trickle:  false,
+            stream: stream
+        })
+        peer.on("signal", data => {
+            this.clientRef.sendMessage('/app/callUser', JSON.stringify({
+                name: name,
+                signalData: data,
+                type: "call"
+            }));
+        })
+        peer.on("stream", stream => {
+            if(this.remoteVideoRef.current) {
+                this.remoteVideoRef.current.srcObject = stream
+            }
+        })
+        callerPeer = peer;
     }
 
-    processAnswer = async msg => {
-        peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
+    //Answering the call
+    answerCall = () => {
+        const { stream, name, receivedSignal } = this.state;
+        if(!stream) {
+            alert("Enable Video to accept call");
+            return;
+        }
+        this.setState({ isCallAnswered: true })
+        const peer = new Peer({
+            initiator: false,
+            trickle: false,
+            stream: stream
+        })
+        peer.on("signal", data => {
+            this.clientRef.sendMessage('/app/callUser', JSON.stringify({
+                name: name,
+                signalData: data,
+                type: "answer"
+            }));
+        })
+        peer.on("stream", stream => {
+            this.remoteVideoRef.current.srcObject = stream;
+        })
+        peer.signal(receivedSignal);
     }
 
-    gotRemoteMediaStream(event) {
-        console.log("got remote media stream", event);
-        remoteStream = event.stream;
-        console.log("remoteStream", remoteStream)
-    }
+    //EndCall
+    // endCall = () => {
+    //     const { joined, isCallAnswered } = this.state;
+    //     this.setName({ joined: false, isCallAnswered: false });
+    //     if(joined) {
+    //         callerPeer.destroy();
+    //     }
+    //     if(isCallAnswered) {
+    //         answeredPeer.destroy();
+    //     }
+    // }
 
-    registerPeerConnectionListeners() {
-        peerConnection.addEventListener('icecandidate', this.handleConnection);
-        peerConnection.addEventListener('addstream', this.gotRemoteMediaStream);
-      }
-
-    handleConnection = event => {
-        const iceConnection = event.target;
-        const iceCandidate = event.candidate;
-
-        if(iceCandidate) {
-            const newIceCandidate = new RTCIceCandidate(iceCandidate);
-            if(iceConnection !== peerConnection) {
-                console.log("coneection success");
-                peerConnection.addIceCandidate(newIceCandidate);
+    //Receiving msg from websocket
+    onMessageReceive = msg => {
+        console.log("message received",msg);
+        const { messages, isCaller } = this.state;
+        if(!msg.type) {
+            var jobs = messages;
+            jobs.push(msg);
+            this.setState({messages: jobs});
+        } else {
+            if(msg.type === "call") {
+                if(!isCaller) {
+                    this.setState({ isCallReceived: true, receiverName: msg.name, receivedSignal: msg.signalData });
+                }
+            } else if(msg.type === "answer"){
+                if(isCaller) {
+                    this.setState({ joined: true })
+                    callerPeer.signal(msg.signalData);
+                }
             }
         }
     }
 
-    process = async msg => {
-        peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
-        const answer = await peerConnection.createAnswer();
-        console.log('Created answer:', answer);
-        await peerConnection.setLocalDescription(answer);
-        this.sendAnswer(answer);
-    }
-
-    sendAnswer = answer => {
-        this.clientRef.sendMessage('/app/answer-all', JSON.stringify({
-            sdp: answer.sdp,
-            type: answer.type
-        }));
-    }
-
-
-
-    joinCall = event => {
-        const { stream } = this.state;
-        event.preventDefault();
-        peerConnection =new RTCPeerConnection(configuration);
-        this.registerPeerConnectionListeners();
-        
-        peerConnection.addStream(stream);
-
-    }
-    
-    createdAnswer(description) {
-      
-        peerConnection.setLocalDescription(description)
-          .then(() => {
-              console.log("set local for remote")
-          })
-      }
-
     render() {
+        const { isCallReceived, receiverName, isCallAnswered } = this.state;
+
         return (
             <div>
                 <CssBaseline />
                 <NameComponent setName={this.setName}/>
                 <div className="align-center">
-                    <h1>Welcome to Global Chat</h1>
+                    <h1>Welcome to Crimson Chat</h1>
                     <br/><br/>
                 </div>
                 <div className="align-center">
@@ -208,46 +211,35 @@ class App extends Component {
                                     onClick={this.sendMessage}>Send</Button>
                 </div>
                 <div className="align-center">
-                    <video id="selfVideo" autoPlay playsInline ref={this.videoRef}/>
+                    <video id="selfVideo" autoPlay playsInline ref={this.userVideoRef}/>
                     <video id="RemoteVideo" autoPlay playsInline ref={this.remoteVideoRef}/>
                 </div>
                 <div className="align-center">
-                <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>} size="large"
-                                    onClick={this.handleOnClick}>Start Video</Button>
-                <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>} size="large"
-                                    onClick={this.handleCall}>Start Call</Button>
-                <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>} size="large"
-                                    onClick={this.joinCall}>Join Call</Button>
+
+                <Button variant="contained" color="primary" size="large"
+                                    onClick={this.getUserMedia}>Enable Video</Button>
+
+                <Button variant="contained" color="primary" endIcon={<Icon><VideocamIcon/></Icon>} size="large"
+                                    onClick={this.startCall}>Start Call</Button>
+
                 </div>
-                <br/><br/>
+                <br/>
+                {isCallReceived && !isCallAnswered && <div>
+                    <div className="align-center">
+                        {receiverName} is calling you
+                    </div>
+                    <div className="align-center">
+                        <Button variant="contained" color="primary" endIcon={<Icon><VideocamIcon/></Icon>} size="large" onClick={this.answerCall}>Accept Call</Button> 
+                    </div>   
+                </div>}
+                <br/>
                 <div className="align-center">
                     {this.displayMessages()}
                 </div>
                 <SockJsClient url='https://crimsonchat.herokuapp.com/websocket-chat/'
-                              topics={['/topic/user','/topic/video', '/topic/answer']}
-                              onConnect={() => {
-                              }}
-                              onDisconnect={() => {
-                              }}
-                              onMessage={(msg) => {
-                                  console.log("message received",msg);
-                                  if(msg.sdp == null) {
-                                    var jobs = this.state.messages;
-                                    jobs.push(msg);
-                                    this.setState({messages: jobs});
-                                  } else {
-                                    if(msg.type === "offer") {
-                                        console.log("offer coming", msg)
-                                        this.process(msg);
-                                    } else {
-                                        console.log("answer coming", msg)
-                                        this.processAnswer(msg);
-                                    }
-                                  }
-                              }}
-                              ref={(client) => {
-                                  this.clientRef = client
-                              }}/>
+                    topics={['/topic/user','/topic/receiveCall', '/topic/answeredCall']}
+                    onMessage={(msg) => { this.onMessageReceive(msg) }}
+                    ref={(client) => { this.clientRef = client }}/>
             </div>
         )
     }
